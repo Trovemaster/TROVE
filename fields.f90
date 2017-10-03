26d9e18079cf4ffe29544f02970d1b0140c55f2e
Jeffrey Salmond <js947@cam.ac.uk>
2017-10-03 16:35:44 +0100

    convert array-of-strings comparison to select statement (7)

--- fields.f90
+++ fields.f90
@@ -2369,12 +2369,13 @@ module fields
              !
              call readu(w)
              !
-             if ( all( trim(w)/=(/'READ','SAVE','APPEND','NONE','DIVIDE','SPLIT'/) ) ) then 
-               !
+             select case (trim(w))
+             case ('READ','SAVE','APPEND','NONE','DIVIDE','SPLIT')
+               continue
+             case default
                write (out,"('FLinput: illegal key in CHECK_POINT :',a)") trim(w)
                stop 'FLinput -illegal key in CHECK_POINT'
-               !
-             endif 
+             end select
              !
              job%IOj0ext_action = trim(w)
              !
