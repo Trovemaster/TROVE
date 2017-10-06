db2994daaf4b3af07ae086d66b3285d60382aac9
Jeffrey Salmond <js947@cam.ac.uk>
2017-10-06 10:55:16 +0100

    !dec$ -> cpp in plasma.F90

--- plasma.F90
+++ plasma.F90
@@ -1,6 +1,6 @@
 module plasma
- 
-!dec$ define plasma_ = 0
+
+#define plasma_ 0
 
 !
 !  Simplistic type-agnostic PLASMA interface
@@ -21,13 +21,13 @@ module plasma
 
   subroutine plasma_diag_dsytrdx(n,a,w,nroots,Ethres_)
     !
-    !dec$ if (plasma_ > 0) 
+#if plasma_ > 0 
       INCLUDE "plasmaf.h"
       integer(ik), parameter :: VEC = PlasmaVec
       integer(ik), parameter :: UPLO = PlasmaLower
       EXTERNAL PLASMA_DSYTRDX
       INTEGER PLASMA_DSYTRDX
-    !dec$ end if
+#endif
     !
     integer         , intent(in)    :: n
     double precision, intent(inout) :: a(n,n)  ! In:  symmetric matrix to be diagonalized
@@ -69,15 +69,15 @@ module plasma
     !
     if (present(Ethres_)) Ethres = Ethres_
     !
-    !dec$ if (plasma_ == 0) 
+#if plasma_ == 0 
        write(out,"('Plasma is not activated, in plasma.f90 please set plasma_ to 1')")
-    !dec$ end if
+#endif
     !
-    !dec$ if (plasma_ > 0)
+#if plasma_ > 0
       !
       !call getsize(N,LDA,nprocs_)
       !
-    !dec$ end if
+#endif
     !  
     !$omp parallel private(tid)
       if (tid==0) then
@@ -184,7 +184,7 @@ module plasma
     ! 
     ! set up my PLASMA_DSYTRDX environmenta, then call the eigensolver
     !
-    !dec$ if (plasma_ > 0)
+#if plasma_ > 0
         !
         call resetcore(corea,corec)
         CALL PRINTARGS(VEC, UPLO, N, LDA, LDQ, COREA, COREB, COREC, NB, IB)
@@ -202,7 +202,7 @@ module plasma
         END IF
         CALL USETPLASMAENV()
         !
-    !dec$ end if
+#endif
     !
     real_end = get_real_time()
     cpu_end  = get_cpu_time ()
