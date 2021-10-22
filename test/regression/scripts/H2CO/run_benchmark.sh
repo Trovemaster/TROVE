a0593f5b44e9fbc4ebfe56a352535ba9c838d6d6
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-10-22 17:47:26 +0100

    Run benchmark with MPI if makefile called with USE_MPI

--- test/regression/scripts/H2CO/run_benchmark.sh
+++ test/regression/scripts/H2CO/run_benchmark.sh
@@ -14,13 +14,19 @@ export OMP_NUM_THREADS=$nproc
 # Ensure stacksize unlimited (for fortran)
 ulimit -d unlimited
 
-LAUNCH="time"
+if [ -n "${USE_MPI}" ]; then
+  echo "MPI enabled"
+  LAUNCH="mpirun -np 4 --mca opal_warn_on_missing_libcuda 0"
+else
+  echo "MPI disabled"
+  LAUNCH="time"
+fi
 
 echo "Time: `date`"
 echo "Current directory: `pwd`"
 
 for name in file{1..12} file_intensity; do
-  $LAUNCH ./$exe < $name.inp > $name.out
+  $LAUNCH ./$exe $name.inp > $name.out
 done
 
 echo "DONE"
