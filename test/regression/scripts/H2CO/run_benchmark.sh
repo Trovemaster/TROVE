42f09e86831be0d19a71aeae5467a77ccd357216
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2021-11-09 10:41:57 +0000

    Remove another non-portable mpirun flag

--- test/regression/scripts/H2CO/run_benchmark.sh
+++ test/regression/scripts/H2CO/run_benchmark.sh
@@ -14,7 +14,7 @@ ulimit -d unlimited
 
 if [[ ${USE_MPI} == 1 ]]; then
   echo "MPI enabled"
-  LAUNCH="time mpirun -np $nproc --mca opal_warn_on_missing_libcuda 0"
+  LAUNCH="time mpirun -np $nproc"
   ./set_io_format.sh enable
   export OMP_NUM_THREADS=1
 else
