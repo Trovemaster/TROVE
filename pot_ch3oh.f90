d3d8590631c4fb521b83bedf7be6f5d930280175
Sergey Yurchenko <sergei.yurchenko@gmail.com>
2024-08-10 21:13:45 +0100

    docs, CH3OH try

--- pot_ch3oh.f90
+++ pot_ch3oh.f90
@@ -409,7 +409,7 @@ function MLpoten_ch3oh_sym(ncoords, natoms, local, xyz, force) result(f)
     !
     xi(12) = tau
     !
-    call ML_symmetry_transformation_CH3OH(nsym,xi,chi,ndeg)
+    call ML_symmetry_transformation_CH3OH_II(nsym,xi,chi,ndeg)
     !
     do iparam = nparam_eq+1,parmax
       !
@@ -445,7 +445,6 @@ function MLpoten_ch3oh_sym(ncoords, natoms, local, xyz, force) result(f)
   end select 
   !  
 end function  MLpoten_ch3oh_sym
- 
  
  
  
@@ -567,5 +566,129 @@ end function  MLpoten_ch3oh_sym
     dst(12,6) =-src(12)+2.0_ark*pi/3.0_ark
     !
   end subroutine ML_symmetry_transformation_CH3OH
+
+
+ subroutine ML_symmetry_transformation_CH3OH_II(nsym,src,dst,ndeg)
+    implicit none
+    !
+    integer(ik),intent(in)    :: nsym,ndeg  ! group operation  
+    real(ark),intent(in)      :: src(1:ndeg)
+    real(ark),intent(out)     :: dst(1:ndeg,6)
+    !
+    real(ark)         :: repres(nsym,ndeg,ndeg),a,b,e,o
+    integer(rk)       :: ioper
+    !
+    if (verbose>=6) write(out,"('ML_symmetry_transformation_CH3OH_II/start')")
+    !
+    a = 0.5_ark ; b = 0.5_ark*sqrt(3.0_ark) ; e = 1.0_ark ; o = 0.0_ark
+    !
+    repres = 0
+    !
+    repres(:,1,1) = 1.0_ark
+    repres(:,2,2) = 1.0_ark
+    !
+    repres(:,6,6) = 1.0_ark
+    !
+    repres(:,12,12) = 1.0_ark
+    !
+    ! E
+    ! r123
+    repres(1,3,3) = 1.0_ark
+    repres(1,4,4) = 1.0_ark
+    repres(1,5,5) = 1.0_ark
+    ! a123
+    repres(1,7,7) = 1.0_ark
+    repres(1,8,8) = 1.0_ark
+    repres(1,9,9) = 1.0_ark
+    !d9
+    repres(1,10,10) = 1.0_ark
+    repres(1,11,11) = 1.0_ark
+    !
+    !C3+/(132)
+    repres(2,3,5) = 1.0_ark
+    repres(2,4,3) = 1.0_ark
+    repres(2,5,4) = 1.0_ark
+    !
+    repres(2,7,9) = 1.0_ark
+    repres(2,8,7) = 1.0_ark
+    repres(2,9,8) = 1.0_ark
+    !
+    repres(2,10,10) = -a
+    repres(2,10,11) = -b
+    repres(2,11,10) =  b
+    repres(2,11,11) = -a
+    !
+    !C3-/(93)
+    !
+    repres(3,3,4) = 1.0_ark
+    repres(3,4,5) = 1.0_ark
+    repres(3,5,3) = 1.0_ark
+    !
+    repres(3,7,8) = 1.0_ark
+    repres(3,8,9) = 1.0_ark
+    repres(3,9,7) = 1.0_ark
+    !
+    repres(3,10,10) = -a
+    repres(3,10,11) =  b
+    repres(3,11,10) = -b
+    repres(3,11,11) = -a
+    !
+    !C2/(23)->(45)
+    !
+    repres(4,3,3) = 1.0_ark
+    repres(4,4,5) = 1.0_ark
+    repres(4,5,4) = 1.0_ark
+    !
+    repres(4,7,7) = 1.0_ark
+    repres(4,8,9) = 1.0_ark
+    repres(4,9,8) = 1.0_ark
+    !
+    repres(4,10,10) =  1.0_ark
+    repres(4,11,11) = -1.0_ark
+    !
+    !C2'/(9)->(34)
+    repres(5,3,4) = 1.0_ark
+    repres(5,4,3) = 1.0_ark
+    repres(5,5,5) = 1.0_ark
+    !
+    repres(5,7,8)  = 1.0_ark
+    repres(5,8,7)  = 1.0_ark
+    repres(5,9,9)  = 1.0_ark
+    !
+    repres(5,10,10) = -a
+    repres(5,10,11) =  b
+    repres(5,11,10) =  b
+    repres(5,11,11) =  a
+    !
+    !(13)->(35)
+    repres(6,3,5) = 1.0_ark
+    repres(6,4,4) = 1.0_ark
+    repres(6,5,3) = 1.0_ark
+    !
+    repres(6,7,9) = 1.0_ark
+    repres(6,8,8) = 1.0_ark
+    repres(6,9,7) = 1.0_ark
+    !
+    repres(6,10,10) = -a
+    repres(6,10,11) = -b
+    repres(6,11,10) = -b
+    repres(6,11,11) =  a
+    !
+    do ioper = 1,nsym
+      dst(:,ioper) = matmul(repres(ioper,:,:),src) 
+    enddo
+    !
+    dst(12,1) = src(12)
+    dst(12,2) = src(12)-2.0_ark*pi/3.0_ark
+    dst(12,3) = src(12)+2.0_ark*pi/3.0_ark
+    dst(12,4) =-src(12)
+    dst(12,5) =-src(12)+2.0_ark*pi/3.0_ark
+    dst(12,6) =-src(12)-2.0_ark*pi/3.0_ark
+    !
+    if (verbose>=6) write(out,"('ML_symmetry_transformation_CH3OH_II/end')")
+    !
+  end subroutine ML_symmetry_transformation_CH3OH_II
+
+
  !
 end module pot_ch3oh
